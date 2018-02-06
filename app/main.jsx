1fc5ddb85341719eaf141057ac763ad5bfbaae3a
Silvio Henrique Ferreira <shferreira@me.com>
2018-02-06 00:06:56 -0200

    Preact

---
+++ app/main.jsx
@@ -0,0 +1,18 @@
+import { h, render, Component } from "preact";
+
+function createClass(obj) {
+  function F(){ Component.call(this); }
+  var p = F.prototype = new Component;
+  for (var i in obj) p[i] = obj[i];
+  return p.constructor = F;
+}
+
+const Hello = createClass({
+  render() {
+    return (<div>Hello, classes!</div>);
+  }
+});
+
+const node = document.querySelector("main");
+
+render(<Hello />, node, node.lastChild);

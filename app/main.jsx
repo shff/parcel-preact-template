1aa4be1038ddd8028d883e3a08c6d548ba911794
Silvio Henrique Ferreira <shferreira@me.com>
2018-06-15 14:52:18 -0300

    Remove class boilerplate

--- app/main.jsx
+++ app/main.jsx
@@ -1,18 +1,5 @@
-import { h, render, Component } from "preact";
-
-function createClass(obj) {
-  function F(){ Component.call(this); }
-  var p = F.prototype = new Component;
-  for (var i in obj) p[i] = obj[i];
-  return p.constructor = F;
-}
-
-const Hello = createClass({
-  render() {
-    return (<div>Hello, Preact!</div>);
-  }
-});
+import { h, render } from "preact";
 
 const node = document.querySelector("main");
 
-render(<Hello />, node, node.lastChild);
+render(<div>Hello, Preact!</div>, node, node.lastChild);
